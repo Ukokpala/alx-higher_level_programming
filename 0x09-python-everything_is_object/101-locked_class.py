2998b0cff29e0fa0f62570109dc991df289d17b9
Ukokpala <okpalaukamka480@gmail.com>
2023-01-03 11:59:11 -0800

    All tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This defines a locked class"""
+
+
+class LockedClass:
+    """
+    Only allows instatiation of an attribute called first_name
+    """
+
+    __slots__ = ["first_name"]
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Secur
