4462b5555c6fe6400c9e9113c9e840182ed509c9
ukokpala <okpalaukamaka480@gmail.com>
2023-03-31 14:26:33 -0700

    alltask

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
-"""
-import sys
-import requests
-
+"""sends a POST request to the passed URL
+with the email as a parameter,
+and finally displays the body of the response."""
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    import sys
+    import requests
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+    response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(response.text)
